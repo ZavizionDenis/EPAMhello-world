5c3cf6fecb8afaf55e37f9b1732110427296d660
Denis Zavizion <denis@zavizion.com>
2019-11-15 20:54:38 +0400

    Create first app "Hello world"

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "World";
+        System.out.println( hello + " " + world + "!");
+    }
+}
